9d98211b3ed43883e59050ad38f505beb8522e04
dasinb0 <94384375+dasinb0@users.noreply.github.com>
2022-08-04 17:30:03 -0300

    Sincronizacion con clever cloud

---
+++ src/test/java/com/portfolio/Diego/Sani/DiegoSaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Diego.Sani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiegoSaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
