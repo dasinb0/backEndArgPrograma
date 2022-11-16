2fe53859b2e337487f4f1af4a8517b368fa06e09
dasinb0 <94384375+dasinb0@users.noreply.github.com>
2022-11-15 23:54:30 -0300

    Cambio cors

--- src/main/java/com/portfolio/Diego/Sani/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/Diego/Sani/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://frontendargentinaprograma.herokuapp.com")
 public class AuthController {
 
     @Autowired
